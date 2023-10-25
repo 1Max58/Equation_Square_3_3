ac535b2fad30e506bcb9632d05fe48b9818e625a
1Max58 <>
2023-10-25 16:32:03 +0300

    User input and basic calculations implemented

--- src/com/example/equationsquare/EquationSquare.java
+++ src/com/example/equationsquare/EquationSquare.java
@@ -1,7 +1,29 @@
 package com.example.equationsquare;
 
+import java.util.Scanner;
+
 public class EquationSquare {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Program will calculate the roots of the quadratic equation ax2 + bx + c = 0");
+        System.out.println("Please enter the coefficients a, b and c.");
+
+        System.out.print("a = ");
+        float a = input.nextFloat();
+
+        System.out.print("b = ");
+        float b = input.nextFloat();
+
+        System.out.print("c = ");
+        float c = input.nextFloat();
+
+        float delta = (float) (Math.pow(b, 2) - 4 * a * c);
+
+        float x1 = (float) ((-b - Math.pow(delta, 0.5)) / (2 * a));
+        float x2 = (float) ((-b + Math.pow(delta, 0.5)) / (2 * a));
+
+        System.out.printf("x1 = %.2f%n", x1);
+        System.out.printf("x2 = %.2f%n", x2);
     }
 }
