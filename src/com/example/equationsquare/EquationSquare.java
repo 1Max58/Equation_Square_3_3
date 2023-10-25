7b4a178ff194a6b166aeee1967c28affe5c28bc8
1Max58 <>
2023-10-25 17:50:38 +0300

    all cases implemented

--- src/com/example/equationsquare/EquationSquare.java
+++ src/com/example/equationsquare/EquationSquare.java
@@ -6,24 +6,52 @@ public class EquationSquare {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Program will calculate the roots of the quadratic equation ax2 + bx + c = 0");
-        System.out.println("Please enter the coefficients a, b and c.");
+        System.out.println("Program will calculate roots of a quadratic equation ax2 + bx + c = 0");
+        System.out.println("Please enter the real coefficients a, b and c.");
 
         System.out.print("a = ");
         float a = input.nextFloat();
 
-        System.out.print("b = ");
-        float b = input.nextFloat();
+        if (a == 0) {
+            System.out.println("The equation with a = 0 is not a quadratic equation.");
+        } else {
+            System.out.print("b = ");
+            float b = input.nextFloat();
 
-        System.out.print("c = ");
-        float c = input.nextFloat();
+            System.out.print("c = ");
+            float c = input.nextFloat();
 
-        float delta = (float) (Math.pow(b, 2) - 4 * a * c);
+            System.out.println();
+            System.out.println("You entered coefficients for the quadratic equation:");
+            System.out.printf("%.2f * x2 + %.2f * x + %.2f = 0%n", a, b, c);
 
-        float x1 = (float) ((-b - Math.pow(delta, 0.5)) / (2 * a));
-        float x2 = (float) ((-b + Math.pow(delta, 0.5)) / (2 * a));
+            float delta = (float) (Math.pow(b, 2) - 4 * a * c);
 
-        System.out.printf("x1 = %.2f%n", x1);
-        System.out.printf("x2 = %.2f%n", x2);
+            int numberOfRoots;
+            if (delta < 0) {
+                numberOfRoots = 0;
+            } else if (delta == 0) {
+                numberOfRoots = 1;
+            } else {
+                numberOfRoots = 2;
+            }
+
+            float x1, x2;
+            switch (numberOfRoots) {
+                case 0:
+                    System.out.println("The equation does not have real roots");
+                    break;
+                case 1:
+                    x1 = -b / 2 * a;
+                    System.out.printf("The equation has a single real root: x1 = %.2f%n", x1);
+                    break;
+                case 2:
+                    float deltaSquareRoot = (float) Math.pow(delta, 0.5);
+                    x1 = (-b - deltaSquareRoot) / (2 * a);
+                    x2 = (-b + deltaSquareRoot) / (2 * a);
+                    System.out.printf("The equation has two real roots: x1 = %.2f, x2 = %.2f%n", x1, x2);
+                    break;
+            }
+        }
     }
 }
